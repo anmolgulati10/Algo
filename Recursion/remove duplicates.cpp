e4920f7b2b09fa175f28919a67d5f31831391381
Anmol Gulati <gulati.anmol10@gmail.com>
2019-12-08 15:42:40 +0530

    first commit

---
+++ Recursion/remove duplicates.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+string character(string str)
+{
+    if(str.length()==0)
+    {
+        return "";
+    }
+    char ch=str[0];
+    string ros=str.substr(1);
+    string result=character(ros);
+    if(ch==result[0])
+    {
+        return result;
+    }
+    else
+    {
+        return ch+result;
+    }
+}
+int main() {
+	string str;
+    cin>>str;
+    cout<<character(str);
+}
