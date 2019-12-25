942ce3cdf3dbc48821205b0c884e288e120e2012
Anmol Gulati <gulati.anmol10@gmail.com>
2019-12-26 03:12:15 +0530

    codes added

---
+++ Recursion/recursion dictionary order larger.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void printls(string str,string ans,string a)
+{
+    if(str.length()==0)
+    {
+        if(ans>a)
+        {
+            cout<<ans<<endl;
+        }
+        return;
+    }
+    int i=0;
+    while(str[i]!='\0')
+    {
+        char ch=str[i];
+        string ls=str.substr(0,i);
+        string rs=str.substr(i+1);
+        string ros=ls+rs;
+        printls(ros,ans+ch,a);
+       // printls(ros,ch+ans);
+        i++;
+    }
+    return;
+}
+
+int main() {
+    string strf;
+    cin>>strf;
+    printls(strf,"",strf);
+}
